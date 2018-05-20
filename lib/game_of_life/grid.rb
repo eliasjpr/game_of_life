ea1a0785645db68b19b9e2f3e8f7f67329f29536
Elias J. Perez <eliasjpr@gmail.com>
2018-05-20 09:29:56 -0400

    [GoL] Build Grid With Living Cells
    Allows to create a grid with a list of living cells.
    
    This will later be use to be able to create generations of "life" where
    a new grid will created with the surviving generation.

--- lib/game_of_life/grid.rb
+++ lib/game_of_life/grid.rb
@@ -2,5 +2,13 @@ require "matrix"
 
 module GameOfLife
   class Grid < Matrix
+    WIDTH = 25
+    HEIGHT = 50
+
+    def self.builds_grid_for(living_cells)
+      build(WIDTH, HEIGHT) do |row, column|
+        living_cells.include?([row, column]) ? true : false
+      end
+    end
   end
 end
--- test/grid_test.rb
+++ test/grid_test.rb
@@ -7,5 +7,11 @@ module GameOfLife
       assert_equal grid.first, [0,0]
       assert_equal grid.count, 25
     end
+
+    def test_it_builds_a_grid_with_cells
+      living_cells = [[5,5]]
+      grid = Grid.builds_grid_for(living_cells)
+      assert_equal true, grid[5,5]
+    end
   end
 end
